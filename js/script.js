2f5d11f7032d4faba4ea4c730d123cb0f36b2ab3
dinozcode <ouroboros4666@gmail.com>
2024-01-23 18:30:57 +0300

    fix next prev buttons

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        prevArrow: '<button type="button" class="slick-prev" aria-label="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next" aria-label="slick-next"><img src="icons/right.svg"></button>',
         autoplay: true,
         autoplaySpeed: 3000,
         responsive: [
